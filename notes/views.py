1d1dee90e829379c0144669fde98e0e3211bc616
Elvandro <elvisamuni22@gmail.com>
2018-08-01 08:46:52 +0300

    Created the view functions

--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .forms import NewCardForm
+from .models import FlashCard
 
 
+
+@login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    texts = FlashCard.objects.all()
+    return render(request, 'index.html', {"texts":texts})
 
-
+@login_required(login_url='/accounts/login/')
 def new_card(request):
-    form = NewCardForm()
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewCardForm(request.POST, request.FILES)
+        if form.is_valid():
+            texts = form.save(commit=False)
+            texts.owner = current_user
+            texts.save()
+            return redirect('home')
+    else:
+        form = NewCardForm()
     return render(request, 'new_card_form.html', {"form":form})
